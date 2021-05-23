ba275ed7650c28435eb4e557cdced7176f9c9e35
INCC-Norms <83819684+INCC-Norms@users.noreply.github.com>
2021-05-23 22:53:43 +0000

    Update __manifest__.py

--- qrcode/__manifest__.py
+++ qrcode/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'web_studio'],
+    'depends': ['base', 'web_studio', 'studio_customization'],
 
     # always loaded
     'data': [
