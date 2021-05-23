f4e6c3f3094392596749c13cedb9853728b6069f
INCC-Norms <83819684+INCC-Norms@users.noreply.github.com>
2021-05-23 21:46:54 +0000

    Update models.py

--- qrcode/models/models.py
+++ qrcode/models/models.py
@@ -4,24 +4,26 @@ from odoo import models, fields, api, _
 import qrcode
 import base64
 from io import BytesIO
-		
+
 class Rapport(models.Model):
-	_inherit = "sale.order"
-	qr_code = fields.Binary("QR Code", attachment=True, store=True)
-	@api.onchange('partner_id')
+	_inherit = 'x_rapport'
+	
+	@api.onchange('x_studio_partner_id')
 	def generate_qr_code(self):
+		base_url = self.env['ir.config_parameter'].get_param('web.base.url')
+		iden = str(self.id).replace('NewId_', '')
+		base_url += '/web#id='+iden+'&view_type=form&model=product.template'
+
 		qr = qrcode.QRCode(
 		    version=1,
 		    error_correction=qrcode.constants.ERROR_CORRECT_L,
-		    box_size=20,
-		    border=8,
+		    box_size=10,
+		    border=4,
 		)
-		qr.add_data(self.name)
+		qr.add_data(base_url)
 		qr.make(fit=True)
 		img = qr.make_image()
 		temp = BytesIO()
 		img.save(temp, format="PNG")
 		qr_image = base64.b64encode(temp.getvalue())
-		self.qr_code = qr_image
-class Rapport(models.Model):
-	_inherit = 'x_rapport'
+		self.x_qr_code = qr_image
