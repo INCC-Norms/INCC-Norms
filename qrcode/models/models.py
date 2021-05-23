d61b384613a2a92ee43aeee7c41583fc39c19622
INCC-Norms <83819684+INCC-Norms@users.noreply.github.com>
2021-05-23 21:51:21 +0000

    Update models.py

--- qrcode/models/models.py
+++ qrcode/models/models.py
@@ -11,8 +11,8 @@ class Rapport(models.Model):
 	@api.onchange('x_studio_partner_id')
 	def generate_qr_code(self):
 		base_url = self.env['ir.config_parameter'].get_param('web.base.url')
-		iden = str(self.id).replace('NewId_', '')
-		base_url += '/web#id='+iden+'&view_type=form&model=product.template'
+		#iden = str(self.id).replace('NewId_', '')
+		#base_url += '/web#id='+iden+'&view_type=form&model=product.template'
 
 		qr = qrcode.QRCode(
 		    version=1,
@@ -20,7 +20,7 @@ class Rapport(models.Model):
 		    box_size=10,
 		    border=4,
 		)
-		qr.add_data(base_url)
+		qr.add_data('test')
 		qr.make(fit=True)
 		img = qr.make_image()
 		temp = BytesIO()
